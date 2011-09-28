019b9532f3853ab6fdc680f578fdecf6299013aa
Alfredo Ribeiro <alfredo@aatecnologia.com.br>
2011-09-28 15:32:34 -0300

    Fix typos on method name, and redirect

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -3,12 +3,12 @@ class ImporterController < ApplicationController
   end
 
   def create
-    if Item.create_multiples_from_cvs(params[:importer][:products_list])
+    if Item.create_multiples_from_csv(params[:importer][:products_list])
       flash[:notice] = "Items successfuly imported"
     else
       flash[:notice] = "There is some problem on the import, please try again"
     end
-    redirect_to importer_path
+    redirect_to new_importer_path
   end
 
 end
