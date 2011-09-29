466db7af2daf9edfb4f0a150646b11847f670016
Alfredo Ribeiro <alfredo@aatecnologia.com.br>
2011-09-29 09:41:02 -0300

    add transaction to .read_csv_and_populate_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,37 +14,40 @@ class Item < ActiveRecord::Base
 
   def self.read_csv_and_populate_items(file_path)
     begin
-      # read CSV file and create items if it don't exist
-      CSV.read(file_path).each_with_index do |item, i|
-        next if i == 0
+      Item.transaction do
+        # read CSV file and create items if it don't exist
+        CSV.read(file_path).each_with_index do |item, i|
+          next if i == 0
 
-        attributes = {
-          :name        => self.sanitize_text(item[0]),
-          #:image_file => item[1] # we could you paperclip here
-          :provider    => self.sanitize_text(item[2]),
-          :price       => self.sanitize_decimal(item[3]),
-          :gender      => self.sanitize_text(item[4]),
-          :category    => self.sanitize_text(item[5]),
-          :description => self.sanitize_text(item[6]),
-          #:sku        => item[] #we don't have sku on csv file
-        }
-        
-        # if the item already exist update its price and description, if not create a new item
-        if Item.where(attributes.select{|k| ![:price, :description].include?(k)}).count > 0
-          db_item = Item.where(attributes.select{|k| ![:price, :description].include?(k)}).first
-          db_item.update_attributes(attributes.select{|k| [:price, :description].include?(k)})
-        else
-          Item.create!(attributes)
+          attributes = {
+            :name        => self.sanitize_text(item[0]),
+            #:image_file => item[1] # we could you paperclip here
+            :provider    => self.sanitize_text(item[2]),
+            :price       => self.sanitize_decimal(item[3]),
+            :gender      => self.sanitize_text(item[4]),
+            :category    => self.sanitize_text(item[5]),
+            :description => self.sanitize_text(item[6]),
+            #:sku        => item[] #we don't have sku on csv file
+          }
+          
+          # if the item already exist update its price and description, if not create a new item
+          if Item.where(attributes.select{|k| ![:price, :description].include?(k)}).count > 0
+            db_item = Item.where(attributes.select{|k| ![:price, :description].include?(k)}).first
+            db_item.update_attributes(attributes.select{|k| [:price, :description].include?(k)})
+          else
+            Item.create!(attributes)
+          end
         end
+        return true
       end
-      return true
+      return false
     rescue
       return false
     end
   end
 
   protected
-    # sanatize decimal numbers, clean any typo before try add it to database
+    # sanitize decimal numbers, clean any typo before try add it to database
     def self.sanitize_decimal(number)
       unless number.is_a?(Float) or number.is_a?(Integer)
         number = number.match(/(\d+\.\d+|\d+)/)[0]
